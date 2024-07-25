66376a64558c2268f8eb796b14c3d5becb0ea048
Sarah Gad <sarahgad298@gmail.com>
2024-07-25 05:41:07 +0300

    added the return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2])
+  return Promise.all([promise1, promise2])
     .then((resolvedvalue) => console.log(`${resolvedvalue[0].body} ${resolvedvalue[1].firstName} ${resolvedvalue[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
