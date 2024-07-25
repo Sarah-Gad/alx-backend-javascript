a7200c3e01634063b318bae128c221d6a78983c5
Sarah Gad <sarahgad298@gmail.com>
2024-07-25 06:26:53 +0300

    00

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const sign = signUpUser(firstName, lastName);
-  const upload = uploadPhoto(fileName);
-  return Promise.allSettled([sign, upload]).then((result) => result.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) => {
+    const results = [];
+    result.forEach((object) => {
+      results.push({
+        status: object.status,
+        value:
+          object.status === 'fulfilled' ? object.value : String(object.reason),
+      });
+    });
+    return results;
+  });
 }
