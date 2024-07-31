d5f5113db74e9e68b0631d4e744c97cd7a216a5e
Sarah Gad <sarahgad298@gmail.com>
2024-07-31 05:53:34 +0300

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  const myarray = [];
+  myarray.push({
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  });
+  myarray.push({
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  });
+  myarray.push({
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  });
+  return myarray;
+}
