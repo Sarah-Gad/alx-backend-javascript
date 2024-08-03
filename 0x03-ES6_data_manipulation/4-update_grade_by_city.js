c5c93d3c8273bcaf033956611f6690f009e66aa5
Sarah Gad <sarahgad298@gmail.com>
2024-08-03 10:50:54 +0300

    updated task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,15 @@
-export default function updateStudentGradeByCity(students, city, grades) {
-  const defaultGrade = { grade: 'N/A' };
-
-  if (students instanceof Array) {
-    return students
-      .filter((student) => student.location === city)
-      .map((student) => ({
-        id: student.id,
-        firstName: student.firstName,
-        location: student.location,
-        grade: (
-          grades.filter((grade) => grade.studentId === student.id).pop()
-          || defaultGrade
-        ).grade,
-      }));
-  }
-  return [];
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  const arrayBcity = array.filter((oneob) => oneob.location === city)
+  const updatedarr = arrayBcity.map((one) => {
+    for (const newg of newGrades) {
+      if (one.id === newg.studentId) {
+        one.grade = newg.grade
+      }
+    }
+    if (!('grade' in one)) {
+      one.grade = "N/A"
+    }
+    return one
+  })
+  return updatedarr
 }
