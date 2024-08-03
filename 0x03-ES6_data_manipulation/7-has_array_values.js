abc33c728595f73786cea2e589862ebb2caf0543
Sarah Gad <sarahgad298@gmail.com>
2024-08-03 14:55:42 +0300

    edit task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,14 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  let flag = 0;
-  for (const elem of array) {
-    if (set.has(elem)) {
-      flag = 0;
-    } else {
-      flag = 1;
-    }
-  }
-  if (flag === 1) {
-    return false;
-  }
-  return true;
+  return array.every((value) => set.has(value));
 }
