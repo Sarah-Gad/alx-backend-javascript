1816334ce47fd5312665f9201d6700fc280d737c
Sarah Gad <sarahgad298@gmail.com>
2024-08-03 15:46:09 +0300

    edit task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,15 @@
 export default function cleanSet(set, startString) {
-  const array = [...set];
-  let restr = '';
-  let flag = false;
-  if (startString.length !== 0) {
-    for (const elem of array) {
-      if (elem.startsWith(startString)) {
-        if (flag) {
-          restr += '-';
-        }
-        restr += elem.slice((startString.length), elem.length);
-        flag = true;
-      }
+  if (!startString) {
+    return '';
+  }
+
+  const result = [];
+
+  for (const elem of set) {
+    if (elem.startsWith(startString)) {
+      result.push(elem.slice(startString.length));
     }
   }
-  return restr;
+
+  return result.join('-');
 }
