b9925d7fc5285f9c9611c0db1ed1dd7a1d954423
Sarah Gad <sarahgad298@gmail.com>
2024-09-18 05:30:58 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+// This file creates a displayMessage fucntion
+module.exports = displayMessage;
+function displayMessage(arg) {
+    console.log(arg)
+}
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This directory contains my tasks for the 0x05-Node_JS_basic project.
