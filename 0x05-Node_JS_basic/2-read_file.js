7ef3f8ac6ca1d66a69a7ca498c70124974ecf667
Sarah Gad <sarahgad298@gmail.com>
2024-09-20 05:34:08 +0300

    fixed ESLint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,34 +3,34 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf-8');
-        parse(data, { columns: true, trim: true }, (err, records) => {
-            if (err) {
-                throw new Error('Error parsing CSV data');
-            }
-            const totalSt = records.length;
-            console.log(`Number of students: ${totalSt}`);
-            let stuCs = 0;
-            let stuSwe = 0;
-            const csAr = [];
-            const sweAr = [];
-            for (let i = 0; i < totalSt; i += 1) {
-                if (records[i].field === 'CS') {
-                    stuCs += 1;
-                    csAr.push(records[i].firstname);
-                }
-                if (records[i].field === 'SWE') {
-                    stuSwe += 1;
-                    sweAr.push(records[i].firstname);
-                }
-            }
-            console.log(`Number of students in CS: ${stuCs}. List: ${csAr.join(', ')}`);
-            console.log(`Number of students in SWE: ${stuSwe}. List: ${sweAr.join(', ')}`);
-        });
-    } catch (err) {
-        throw new Error('Cannot load the database');
-    }
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    parse(data, { columns: true, trim: true }, (err, records) => {
+      if (err) {
+        throw new Error('Error parsing CSV data');
+      }
+      const totalSt = records.length;
+      console.log(`Number of students: ${totalSt}`);
+      let stuCs = 0;
+      let stuSwe = 0;
+      const csAr = [];
+      const sweAr = [];
+      for (let i = 0; i < totalSt; i += 1) {
+        if (records[i].field === 'CS') {
+          stuCs += 1;
+          csAr.push(records[i].firstname);
+        }
+        if (records[i].field === 'SWE') {
+          stuSwe += 1;
+          sweAr.push(records[i].firstname);
+        }
+      }
+      console.log(`Number of students in CS: ${stuCs}. List: ${csAr.join(', ')}`);
+      console.log(`Number of students in SWE: ${stuSwe}. List: ${sweAr.join(', ')}`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
