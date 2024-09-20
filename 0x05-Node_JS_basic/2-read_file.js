30f89d8f4cb2f14db165fb09e67da2584cfe9dce
Sarah Gad <sarahgad298@gmail.com>
2024-09-20 05:32:54 +0300

    tried the async fucntion

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,42 +1,36 @@
 // This module reads from a file
+const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const countStudents = (dataPath) => {
-  if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
-  }
-  if (!fs.statSync(dataPath).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
-    .trim()
-    .split('\n');
-  const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
-
-  for (const line of fileLines.slice(1)) {
-    const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-    const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf-8');
+        parse(data, { columns: true, trim: true }, (err, records) => {
+            if (err) {
+                throw new Error('Error parsing CSV data');
+            }
+            const totalSt = records.length;
+            console.log(`Number of students: ${totalSt}`);
+            let stuCs = 0;
+            let stuSwe = 0;
+            const csAr = [];
+            const sweAr = [];
+            for (let i = 0; i < totalSt; i += 1) {
+                if (records[i].field === 'CS') {
+                    stuCs += 1;
+                    csAr.push(records[i].firstname);
+                }
+                if (records[i].field === 'SWE') {
+                    stuSwe += 1;
+                    sweAr.push(records[i].firstname);
+                }
+            }
+            console.log(`Number of students in CS: ${stuCs}. List: ${csAr.join(', ')}`);
+            console.log(`Number of students in SWE: ${stuSwe}. List: ${sweAr.join(', ')}`);
+        });
+    } catch (err) {
+        throw new Error('Cannot load the database');
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
-  }
-
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
-  console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
-  }
-};
+}
 
 module.exports = countStudents;
