ae0a655ea3db71d37aba7ce0d17a89de18d81bf1
Sarah Gad <sarahgad298@gmail.com>
2024-09-20 04:57:10 +0300

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,46 +1,35 @@
-const fs = require('fs');
+// This module reads from a file
+const { parse } = require('csv-parse');
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
-const countStudents = (dataPath) => {
-  if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
-  }
-  if (!fs.statSync(dataPath).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
-    .trim()
-    .split('\n');
-  const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
-
-  for (const line of fileLines.slice(1)) {
-    const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-    const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+function countStudents(path) {
+  const fs = require('fs');
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
-  }
-
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
-  console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
-  }
-};
-
+    parse(data, { columns: true, trim: true }, (err, records) => {
+      if (err) {
+        throw new Error('Error parsing CSV data');
+      }
+      total_st = records.length;
+      console.log(`Number of students: ${total_st}`);
+      let stu_cs = 0;
+      let stu_swe = 0;
+      const cs_ar = [];
+      const swe_ar = [];
+      for (let i = 0; i < total_st; i++) {
+        if (records[i].field == 'CS') {
+          stu_cs++;
+          cs_ar.push(records[i].firstname);
+        }
+        if (records[i].field == 'SWE') {
+          stu_swe++;
+          swe_ar.push(records[i].firstname);
+        }
+      }
+      console.log(`Number of students in CS: ${stu_cs}. List: ${cs_ar.join(', ')}`);
+      console.log(`Number of students in SWE: ${stu_swe}. List: ${swe_ar.join(', ')}`);
+    });
+  });
+}
 module.exports = countStudents;
